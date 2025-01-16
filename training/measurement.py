9bdbfdf576022b8a44f402c1c91e81141c57cc98
Orbán Gábor <118307686+GaborOrbanDev@users.noreply.github.com>
2025-01-16 17:21:48 +0100

    create measurment class

--- training/measurement.py
+++ training/measurement.py
@@ -1,36 +1,63 @@
-# %% Putting file to absolute path to be able to call schema packages
 import sys
 import os
 import time
+import pandas as pd
+from langchain_community.callbacks import get_openai_callback
+from langgraph.graph.state import CompiledStateGraph
+from tqdm import tqdm
+from pydantic import BaseModel, Field
+
+# Add the parent directory to the sys path to import the schemas
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-#%% importing libraries
-from langchain_community.callbacks import get_openai_callback
 from schemas.agent_state_classes import AgentInput, Task
 from schemas.cot_sc_schema import CoTSCAgent
 from schemas.cot_schema import CoTAgent
 from schemas.cot_w_sr_schema import CoTwSRAgent
 from schemas.spp_schema import SPPAgent
 from schemas.tot_schema import ToTAgent
-import pandas as pd
 
-#%% reading the data
+
+class Measurement(BaseModel):
+    cognitive_schema: str = Field(..., description="Label of the used cognitive schema")
+    task_id: int = Field(..., description="ID of the task")
+    task: str = Field(..., description="Text of the task")
+    subject_of_task: str = Field(..., description="Subject of the task")
+    accuracy: int = Field(..., description="Accuracy of the cognitive schema", ge=0, le=1)
+    inference_time: float = Field(..., description="Inference time of the cognitive schema")
+    total_tokens: int = Field(..., description="Total tokens used in the inference")
+    prompt_tokens: int = Field(..., description="Tokens used in the prompt")
+    completion_tokens: int = Field(..., description="Tokens used in the completion")
+    cost: float = Field(..., description="Cost of the inference")
+
+
 data = pd.read_csv("./training/mmlu_all_w_id.csv", sep=";")
 
-#%% defining the callback
-agent = ToTAgent().create_agent()
+def measure(task: dict, label: str, agent: CompiledStateGraph) -> None:
+    task_text = f"**Question**: {task["question"]} | **Answer choices**: {task["choices"]}"
+    choice_index = task["answer"]
+    task_id = task["task_id"]
+    subject_of_task = task["subject_of_task"]
+        
+    with get_openai_callback() as cb:
+        start_time = time.perf_counter()
+        response: dict[str, Task] = agent.invoke(AgentInput(task=Task(description=task_text)))
+        end_time = time.perf_counter()
+        inference_time = end_time - start_time
 
-with get_openai_callback() as cb:
-    task_text="""Which one of the following is the most appropriate definition of a 99% confidence interval? [ "99% of the time in repeated samples, the interval would contain the true value of the parameter", "99% of the time in repeated samples, the interval would contain the estimated value of the parameter", "99% of the time in repeated samples, the null hypothesis will be rejected", "99% of the time in repeated samples, the null hypothesis will not be rejected when it was false" ]"""
-    agent_input = AgentInput(task=Task(description=task_text))
-    start_time = time.perf_counter()
-    response = agent.invoke(agent_input)
-    end_time = time.perf_counter()
-    inference_time = end_time - start_time
-    print(response)
-    print("====================================")
-    print(f"Inference Time: {inference_time}")
-    print(f"Total Tokens: {cb.total_tokens}")
-    print(f"Input Tokens: {cb.prompt_tokens}")
-    print(f"Output Tokens: {cb.completion_tokens}")
-    print(f"Cost: {cb.total_cost}")
+        accuracy = 1 if response["task"].solution.index == choice_index else 0
+
+        measurement = Measurement(
+            cognitive_schema=label,
+            task_id=task_id,
+            task=task_text,
+            subject_of_task=subject_of_task,
+            accuracy=accuracy,
+            inference_time=inference_time,
+            total_tokens=cb.total_tokens,
+            prompt_tokens=cb.prompt_tokens,
+            completion_tokens=cb.completion_tokens,
+            cost=cb.total_cost
+        )
+
+    
